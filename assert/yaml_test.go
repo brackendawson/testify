2ad726ff1075285e4b65f84348e136c3f843c196
Bracken Dawson <abdawson@gmail.com>
2025-05-07 14:30:15 +0100

    difference with "!!0", goccy thinks it's nil but gopkg.in/yaml.v3 thinks it's "".

--- assert/yaml_test.go
+++ assert/yaml_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert/yaml"
 )
 
 type nopTestingT struct{}
@@ -16,6 +17,20 @@ key: value # as yaml
 `, `{"key":"value"}`)
 	f.Add(`1: one`, `2: two`)
 	f.Fuzz(func(t *testing.T, expexted, actual string) {
+		var v any
+		if err := yaml.UnmarshalOld([]byte(expexted), &v); err != nil {
+			t.Skip()
+		}
+		if err := yaml.UnmarshalOld([]byte(actual), &v); err != nil {
+			t.Skip()
+		}
+		if err := yaml.UnmarshalNew([]byte(expexted), &v); err != nil {
+			t.Skip()
+		}
+		if err := yaml.UnmarshalNew([]byte(actual), &v); err != nil {
+			t.Skip()
+		}
+
 		gotOld := assert.YAMLEqOld(nopTestingT{}, expexted, actual)
 		gotNew := assert.YAMLEqNew(nopTestingT{}, expexted, actual)
 		assert.Equal(t, gotOld, gotNew)
